1d954d2e8e8cc391494629665a00c9a8d1cc7616
Varun Sundar <varun19299@gmail.com>
2019-02-17 18:27:57 +0530

    Initial: Added files, generated y vector.

---
+++ README.md
@@ -0,0 +1,13 @@
+# OFDM System Simulation
+
+## Install Packages
+
+`pip install -r requirements.txt`
+
+## TO-DO
+
+1. [x] Generate y vector.
+2. [ ] Use LSE to get h.
+3. [ ] Sparsity Q2.
+4. [ ] Guard Band of 200 each side.
+5. [ ] Constrained LSE formulation.
---
+++ sim.py
@@ -0,0 +1,76 @@
+import numpy as np
+from sacred import Experiment
+import cmath
+
+ex = Experiment("simulation")
+
+@ex.config
+def config():
+    x_ii = np.array([1 + 1j,-1 + 1j,1 - 1j,-1 - 1j], dtype= np.complex_)    # diag of X belongs to
+    N = 1024                          # no of freqs
+    n = 16                            # no of channel taps
+
+    sigma_real = 0.1
+    sigma_imag = 0.1
+
+    d = 0.2                           # Decay factor
+
+@ex.capture
+def get_F(N, n):
+    '''
+    Generate IDFT Matrix
+    : N : No of frequencies
+    : n : channel taps 
+    
+    Returns:
+    Float Matrix: N*n
+    '''
+    F = np.zeros((N,n), dtype = np.complex_) 
+    for i in range(N):
+        for j in range(n):
+            F[i, j] = cmath.rect(1, 2*np.pi*i*j/1024)
+
+    return F
+
+@ex.capture
+def get_X(N, x_ii):
+    '''
+    Generates diagonal matrix with known symbols
+    '''
+    assert not (N%4), "N must be divisible by 4"
+
+    xx = np.tile(x_ii, N//4)
+    X = np.diag(xx)
+    return X
+
+@ex.capture
+def get_h(n, d, sigma_real, sigma_imag):
+    '''
+    n tap time domain channel vector
+
+    Generates h vec according to exponentially decaying power-delay profile
+    : sigma_real : variance of zero mean gaussian dist for real part
+    : sigma_imag : variance of zero mean gaussian dist for imag part
+    '''
+    a = np.random.normal(0, np.sqrt(1/2), size = n)
+    b = np.random.normal(0, np.sqrt(1/2), size = n)
+    h = np.exp(-d*np.arange(n)) * (a + b*1j)
+    h = h/np.linalg.norm(h)
+    return h
+
+@ex.capture
+def get_y():
+    '''
+    Generates y vector
+
+    Returns: 
+    : y of size N 
+    '''
+    h = get_h()
+    F = get_F()
+    X = get_X()
+    return np.matmul(np.matmul(X,F),h)
+
+@ex.automain
+def main():
+    print(get_y())
