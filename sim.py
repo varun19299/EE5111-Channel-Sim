7de5b298f375f461b9f2e4cc871e51bdb09028a8
Rishhanth Maanav V <rishanthmaanav@gmail.com>
2019-02-23 23:16:14 +0530

    Add sparsity information (need to clean up a bit

--- sim.py
+++ sim.py
@@ -44,7 +44,7 @@ def get_X(N, x_ii):
     return X
 
 @ex.capture
-def get_h(n, d, sigma_real, sigma_imag):
+def get_h(n, d=0.2, sigma2_real=0.5, sigma2_imag=0.5):
     '''
     n tap time domain channel vector
 
@@ -52,14 +52,28 @@ def get_h(n, d, sigma_real, sigma_imag):
     : sigma_real : variance of zero mean gaussian dist for real part
     : sigma_imag : variance of zero mean gaussian dist for imag part
     '''
-    a = np.random.normal(0, np.sqrt(1/2), size = n)
-    b = np.random.normal(0, np.sqrt(1/2), size = n)
-    h = np.exp(-d*np.arange(n)) * (a + b*1j)
-    h = h/np.linalg.norm(h)
+    a = np.random.normal(0, np.sqrt(sigma2_real), size = n)
+    b = np.random.normal(0, np.sqrt(sigma2_imag), size = n)
+    p = np.exp(-d*np.arange(n))
+    h = p * (a + b*1j)
+    h = h/np.linalg.norm(p)
     return h
 
 @ex.capture
-def get_y():
+def get_h_sparse(n, zero_ind,d=0.2, sigma2_real=0.5, sigma2_imag=0.5):
+    '''
+    n tap time domain channel vector
+
+    Generates h vec according to exponentially decaying power-delay profile
+    : sigma_real : variance of zero mean gaussian dist for real part
+    : sigma_imag : variance of zero mean gaussian dist for imag part
+    '''
+    h = get_h(n,d,sigma2_real,sigma2_imag)
+    h[zero_ind] = 0 + 0j
+    return h
+
+@ex.capture
+def get_y(X,F,h):
     '''
     Generates y vector
 
@@ -67,20 +81,46 @@ def get_y():
     : y of size N 
     : h_actual of size n
     '''
-    h = get_h()
-    F = get_F()
-    X = get_X()
-    return np.matmul(np.matmul(X,F),h), h
+    return np.matmul(np.matmul(X,F),h)
+
+@ex.capture
+def get_A(X,F):
+    return np.matrix(np.matmul(X,F))
+
+@ex.capture
+def get_estim_h(A,y):
+    h_est = np.linalg.inv(np.matmul(A.getH(),A))
+    h_est = np.matmul(h_est,A.getH())
+    h_est = np.matmul(h_est, y)
+    return h_est
+
+@ex.capture
+def get_estim_sparse_h(A,y,zero_ind):
+    h_est = get_estim_h(A,y)
+    L = h_est.shape[1]
+    r = len(zero_ind)
+    C_m = np.eye(L)
+    C_m = np.matrix(C_m[zero_ind,:])
+    c_b = np.zeros([r,1])
+    tempM = np.matmul(np.linalg.inv(np.matmul(A.getH(),A)),C_m.getH())
+    constraint = np.matmul(C_m,h_est.T) - c_b
+    h_sparse_estim = h_est - np.matmul(np.matmul(tempM,np.linalg.inv(np.matmul(C_m,tempM))),constraint).T
+    return h_sparse_estim
+
 
 @ex.automain
 def main():
-    y, h_act = get_y()
-    X = get_X()
-    F = get_F()
-    A = np.matmul(X,F)      # usual y = Ah + n
-    h_est = np.linalg.inv(np.matmul(A.T,A))
-    h_est = np.matmul(h_est,A.T)
-    h_est = np.matmul(h_est, y)
+    N = 1024
+    L = 16
+    x_ii = np.array([1+1j,-1+1j,1-1j,-1-1j])
+    X = get_X(N,x_ii)
+    non_zero_ind = [2,5,7,9,10,12]
+    zero_ind = np.delete(np.arange(L),non_zero_ind)
+    h_act = get_h_sparse(L,zero_ind)
+    F = get_F(N,L)
+    y = get_y(X,F,h_act)
+    A = get_A(X,F)
+    h_est = get_estim_sparse_h(A,y,zero_ind)    
     error = h_act - h_est
     print(f"h actual {h_act}")
     print(f"h estimated {h_est}")
