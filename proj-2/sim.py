94278b124527256820f4bc6cd9d3c876dd7a8bd0
lokesh.karpagam@gmail.com <lokesh.karpagam@gmail.com>
2019-03-12 22:26:42 +0530

    added cauchy

--- proj-2/sim.py
+++ proj-2/sim.py
@@ -3,23 +3,24 @@ from sacred import Experiment
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from functools import partial
+from scipy import optimize
 
 ex = Experiment("MLE-dist-simulation")
 
 @ex.config
 def config():
     A = 1
-    N = 10      # No of data points
+    N = 1      # No of data points
     n = 100     # No of experiments to repeat over
+    a0 = 0.5
+    gamma = 0.52999894
 
 @ex.capture
 def plot_stats(distribution, estimator,range_ll, A, N, n):
-    pbar = tqdm(range(n))
     estimator_ll = []
-    for i in pbar:
+    for i in tqdm(range(n)):
         data = distribution(size = N)
         estimator_ll.append(estimator(data))
-        pbar.set_description(f"")
 
     estimator_ll = np.array(estimator_ll)
     estimator_mean = np.mean(estimator_ll)
@@ -30,10 +31,34 @@ def plot_stats(distribution, estimator,range_ll, A, N, n):
 
     return estimator_mean, estimator_var
 
+@ex.capture
+def cauchy_likelihood(a, x, gamma):
+    '''
+    Function that defines the derivative likelihood for cauchy distribution
+    '''
+    val = 0
+    for i in x:
+        val = val + (i-a)/(1 + ((i-a)/gamma)**2)
+    return val
+
+@ex.capture
+def cauchy_dist(size, A, gamma):
+    '''
+    Function to sample from the cauchy distribution
+    '''
+    return A + np.random.standard_cauchy(size=(10000))*gamma
+
+@ex.capture
+def find_root(x, a0, gamma, f = cauchy_likelihood):
+    '''
+    Using Newton-Raphson to find to root
+    '''
+    res = optimize.newton(f, a0, args=(x, gamma))
+    return res
+
 
 @ex.automain
 def main(_run):
-    distribution = np.random.normal
-    distribution = partial(distribution, loc = 1, scale = 1)
-    mean, var = plot_stats(distribution, np.mean, range_ll=[0,2])
+    distribution = cauchy_dist
+    mean, var = plot_stats(distribution, find_root, range_ll=[0,2])
     print(mean, var)
